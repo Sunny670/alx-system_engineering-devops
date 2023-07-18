44c0d36f92da43c5c3bab3a26af7dcfa4c815118
Sunny670 <sunny.thanki13@gmail.com>
2023-07-18 18:01:15 +0000

    done

--- create_grant_prv.sql
+++
@@ -1,4 +0,0 @@
--- A sql script to create a user holberton_user with password "projectcorrection280hbtn"
--- Also granting this user replication priviledge
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
