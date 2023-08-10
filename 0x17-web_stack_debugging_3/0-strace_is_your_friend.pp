2dd2ae2a9f3cff2cc2d2ee64512b0958e6f45bc1
Sunny670 <sunny.thanki13@gmail.com>
2023-08-10 17:06:10 +0300

    web stack

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
-# A puppet manuscript to replace a line in a file on a server
+# Solve 500 error on apache
 
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+exec { 'fix':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
-.
